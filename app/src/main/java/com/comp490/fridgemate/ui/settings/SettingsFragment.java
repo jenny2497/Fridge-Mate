dce0c27ce8e92885a8c937561460fde55525f1ea
jenny2497 <79676021+jenny2497@users.noreply.github.com>
2023-04-25 12:26:09 -0700

    setting page

--- app/src/main/java/com/comp490/fridgemate/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/comp490/fridgemate/ui/settings/SettingsFragment.java
@@ -1,28 +1,40 @@
 package com.comp490.fridgemate.ui.settings;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.comp490.fridgemate.LoginActivity;
 import com.comp490.fridgemate.MainActivity;
 import com.comp490.fridgemate.R;
 import com.comp490.fridgemate.databinding.FragmentSettingsBinding;
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SettingsFragment extends Fragment {
 
     private FragmentSettingsBinding binding;
+    TextView nam, email;
+    String userEmail, userName;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -31,6 +43,17 @@ public class SettingsFragment extends Fragment {
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            userName=user.getDisplayName();
+            userEmail=user.getEmail();
+
+            nam = root.findViewById(R.id.Name);
+            nam.setText(userName);
+            email = root.findViewById(R.id.Email);
+            email.setText(userEmail);
+        }
 
         final Button button = root.findViewById(R.id.sign_out_button);
 
@@ -46,8 +69,6 @@ public class SettingsFragment extends Fragment {
                         });
             }
         });
-
-
         return root;
     }
 
@@ -56,6 +77,4 @@ public class SettingsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
-
 }
